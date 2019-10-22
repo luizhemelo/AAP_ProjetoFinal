5f95dcdb2b25c1b18d1624208058f6360a0c9754
Otávio Augusto Silva <otavio.silva@dcc.ufmg.br>
2019-10-22 17:54:39 -0300

    Fixed bug in PrunableDense and added docstring
    Fixed bug in kernel and bias property, now working as expected. Also, added docstring to class and functions.

--- Lottery Ticket Hypothesis/lottery_ticket_hypothesis.py
+++ Lottery Ticket Hypothesis/lottery_ticket_hypothesis.py
@@ -2,9 +2,14 @@ import tensorflow
 from tensorflow.keras import layers
 
 devices = tensorflow.config.experimental.list_physical_devices("GPU")
-tensorflow.config.experimental.set_memory_growth(devices[0], True)
+for i in devices:
+	tensorflow.config.experimental.set_memory_growth(i, True)
+#tensorflow.config.experimental.set_memory_growth(devices[0], True)
 
 class PrunableDense(layers.Dense):
+	"""
+	Custom keras.layer class of a prunable Dense layer.
+	"""
 	def __init__(self, *args, **kwargs):
 		super(PrunableDense, self).__init__(*args, **kwargs)
 		self.trainable_channels = None
@@ -15,6 +20,12 @@ class PrunableDense(layers.Dense):
 		self._bias2 = None
 
 	def build(self, input_shape):
+		"""
+		Custom build function that creates trainable channels Tensor and trainable bias Tensor.
+		Parameters
+		-------------
+		input_shape: Shape of the layer input
+		"""
 		last_dim = input_shape[-1]
 		self._kernel1 = self.add_weight("kernel1", shape=(last_dim, self.units), initializer=self.kernel_initializer, regularizer=self.kernel_regularizer, constraint=self.kernel_constraint, dtype=self.dtype, trainable=True)
 		self._kernel2 = tensorflow.zeros((last_dim, self.units))
@@ -26,24 +37,40 @@ class PrunableDense(layers.Dense):
 
 	@property
 	def kernel(self):
-		a = self.trainable_channels * self._kernel1 + (1 - self.trainable_channels * self._kernel2)
-		return a
+		"""
+		Custom kernel property that returns only trainable channels.
+		"""
+		return self.trainable_channels * self._kernel1 + (1 - self.trainable_channels) * self._kernel2
 
 	@property
 	def bias(self):
+		"""
+		Custom bias property that returns only trainable bias.
+		"""
 		if not self.use_bias:
 			return None
 		else:
-			a = self.trainable_bias * self._bias1 + (1 - self.trainable_bias * self._bias2)
-			return a
+			return self.trainable_bias * self._bias1 + (1 - self.trainable_bias) * self._bias2
 
 	def prune_kernel(self, to_be_pruned):
+		"""
+		Prune the network layer on specific weights.
+		Parameters
+		---------------
+		to_be_pruned: NumPy Array or Tensor of shape=kernel.shape
+		"""
 		new_pruned = 1 - tensorflow.maximum((1 - to_be_pruned) - (1 - self.trainable_channels), 0)
 		new_pruned_weights = (1 - new_pruned) * self._kernel1
 		self._kernel2 += new_pruned_weights
 		self.trainable_channels *= to_be_pruned
 
 	def prune_bias(self, to_be_pruned):
+		"""
+		Prune the bias on specific weights.
+		Parameters
+		--------------
+		to_be_pruned: NumPy Array or Tensor with shape=kernel.shape
+		"""
 		assert (self.use_bias)
 		new_pruned = 1 - tensorflow.maximum((1 - to_be_pruned) - (1 - self.trainable_bias), 0)
 		new_pruned_bias = (1 - new_pruned) * self._bias1
