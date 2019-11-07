79fc0471e891b8ffc8a4d6710ec6e6acb5ee2a94
Otávio Augusto Silva <otavio.silva@dcc.ufmg.br>
2019-11-07 15:05:51 -0300

    Code rewritting for better reading

--- Lottery Ticket Hypothesis/lottery_ticket_hypothesis.py
+++ Lottery Ticket Hypothesis/lottery_ticket_hypothesis.py
@@ -32,12 +32,12 @@ class PrunableDense(layers.Dense):
 		"""
 		last_dim = input_shape[-1]
 		self._kernel1 = self.add_weight("kernel1", shape=(last_dim, self.units), initializer=self.kernel_initializer, regularizer=self.kernel_regularizer, constraint=self.kernel_constraint, dtype=self.dtype, trainable=True)
-		self._kernel2 = tensorflow.zeros((last_dim, self.units))
-		self.trainable_channels = tensorflow.ones((last_dim, self.units), dtype=tensorflow.uint8)
+		self._kernel2 = tensorflow.zeros((last_dim, self.units), dtype=self.dtype)
+		self.trainable_channels = tensorflow.ones((last_dim, self.units), dtype=self.dtype)
 		if self.use_bias:
 			self._bias1 = self.add_weight("bias", shape=(self.units,), initializer=self.bias_initializer, regularizer=self.bias_regularizer, constraint=self.bias_constraint, dtype=self.dtype, trainable=True)
-			self._bias2 = tensorflow.zeros((self.units,))
-			self.trainable_bias = tensorflow.ones((self.units,), dtype=tensorflow.uint8)
+			self._bias2 = tensorflow.zeros((self.units,), dtype=self.dtype)
+			self.trainable_bias = tensorflow.ones((self.units,), dtype=self.dtype)
 		self.built = True
 
 	@property
@@ -45,8 +45,7 @@ class PrunableDense(layers.Dense):
 		"""
 		Custom kernel property that returns only trainable channels.
 		"""
-		t = tensorflow.cast(self.trainable_channels, dtype=self.dtype)
-		return t * self._kernel1 + (1 - t) * self._kernel2
+		return self.trainable_channels * self._kernel1 + (1 - self.trainable_channels) * self._kernel2
 
 	@property
 	def bias(self):
@@ -56,8 +55,7 @@ class PrunableDense(layers.Dense):
 		if not self.use_bias:
 			return None
 		else:
-			t = tensorflow.cast(self.trainable_bias, dtype=self.dtype)
-			return t * self._bias1 + (1 - t) * self._bias2
+			return self.trainable_bias * self._bias1 + (1 - self.trainable_bias) * self._bias2
 
 	def save_kernel(self):
 		self.saved_W = tensorflow.identity(self.kernel)
@@ -80,11 +78,11 @@ class PrunableDense(layers.Dense):
 		---------------
 		to_be_pruned: NumPy Array or Tensor of shape=kernel.shape with values in {0,  1} indicating which weights to keep (1) and which to drop (0).
 		"""
-		t = tensorflow.cast(to_be_pruned, dtype=tensorflow.float32)
-		new_pruned = 1 - tensorflow.maximum((1 - t) - (1 - tensorflow.cast(self.trainable_channels, dtype=tensorflow.float32)), 0)
+		t = tensorflow.cast(to_be_pruned, dtype=self.dtype)
+		new_pruned = 1 - tensorflow.maximum((1 - t) - (1 - self.trainable_channels), 0)
 		new_pruned_weights = (1 - new_pruned) * self._kernel1
 		self._kernel2 += new_pruned_weights
-		self.trainable_channels *= tensorflow.cast(t, dtype=tensorflow.uint8)
+		self.trainable_channels *= t
 
 	def prune_bias(self, to_be_pruned):
 		"""
@@ -95,8 +93,8 @@ class PrunableDense(layers.Dense):
 		"""
 		assert (self.use_bias)
 		
-		t = tensorflow.cast(to_be_pruned, dtype=tensorflow.float32)
-		new_pruned = 1 - tensorflow.maximum((1 - t) - (1 - tensorflow.cast(self.trainable_bias, dtype=tensorflow.float32)), 0)
+		t = tensorflow.cast(to_be_pruned, dtype=self.dtype)
+		new_pruned = 1 - tensorflow.maximum((1 - t) - (1 - self.trainable_bias), 0)
 		new_pruned_bias = (1 - new_pruned) * self._bias1
 		self._bias2 += new_pruned_bias
-		self.trainable_bias *= tensorflow.cast(t, dtype=tensorflow.uint8)
+		self.trainable_bias *= t
